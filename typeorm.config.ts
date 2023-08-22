d89e72077e44edc8c2dd09b9cf63f90cdf1a90d7
october03 <season_autumn@kakao.com>
2023-08-22 12:25:22 +0900

    connect nest to postgres

--- typeorm.config.ts
+++ typeorm.config.ts
@@ -8,4 +8,5 @@ export const typeOrmConfig: TypeOrmModuleOptions = {
   password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DB,
   entities: [__dirname + '/../**/*.entity.{js,ts}'],
+  synchronize: false,
 };
