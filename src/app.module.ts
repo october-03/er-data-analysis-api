99d9c3e5125ac20291ecae1cbdb34b348f756e64
october03 <season_autumn@kakao.com>
2023-08-25 10:10:43 +0900

    Bull 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,18 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from 'typeorm.config';
+import { BullModule } from '@nestjs/bull';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmConfig)],
+  imports: [
+    TypeOrmModule.forRoot(typeOrmConfig),
+    BullModule.forRoot({
+      redis: { host: 'redis', port: Number(process.env.REDIS_PORT) },
+    }),
+    BullModule.registerQueue({
+      name: 'erApiQueue',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
