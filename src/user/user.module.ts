f104ca8364e29f6529480626a79c3b028feb46cd
october03 <season_autumn@kakao.com>
2023-08-25 15:10:19 +0900

    module consumer 연결

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import { AppConsumer } from 'src/app.consumer';
 
 @Module({
   controllers: [UserController],
-  providers: [UserService]
+  providers: [UserService, AppConsumer],
 })
 export class UserModule {}
